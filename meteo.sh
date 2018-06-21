3e893193a4caf3cdbc7eab664f6320200ae061f2
Benjamin Mosnier <bm@kappuccino.org>
2018-06-21 14:33:53 +0200

    Fix recrop

--- meteo.sh
+++ meteo.sh
@@ -9,6 +9,6 @@ for city in "$@"
 do
     echo "🔥 $city"    
 	node index.js $city
-	convert "images/meteo-$city.png" -crop 632x150+0+310 "images/meteo-$city-$DATE.png"
+	convert "images/meteo-$city.png" -crop 632x130+0+315 "images/meteo-$city-$DATE.png"
 	rm -f "images/meteo-$city.png"
 done

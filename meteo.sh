63c082bf6c298e8da60e55afc262ceaf2e4df93a
Benjamin Mosnier <bm@kappuccino.org>
2019-03-07 09:42:33 +0100

    Crop settings

--- meteo.sh
+++ meteo.sh
@@ -10,7 +10,7 @@ for city in "$@"
 do
     echo "Meteo: $city"
 	docker run --shm-size 1G --rm --env CITY=$city -v "$PWD":/app alekzonder/puppeteer:latest
-	convert "images/meteo-$city.png" -crop 632x120+0+355 "images/meteo-$city-$DATE.png"
+	convert "images/meteo-$city.png" -crop 632x120+0+335 "images/meteo-$city-$DATE.png"
 	rm -f "images/meteo-$city.png"
 done
 
